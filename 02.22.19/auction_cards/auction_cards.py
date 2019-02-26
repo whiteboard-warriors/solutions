90d8f81013c5048d2253e439c4a555bc896f8a16
Jordan Baucke <jordan.baucke@gmail.com>
2019-02-25 21:05:50 -0800

    auction_cards, coin_toss, and rock_paper_scissors in python

--- 02.22.19/auction_cards/auction_cards.py
+++ 02.22.19/auction_cards/auction_cards.py
@@ -1,7 +1,50 @@
-#You are managing an auction with 650 bidders, each bidder is given a bidding card with their number printed on it. However, the numbers 0,1,6,8,9 all look the same upside-down or right-side up.
+#!/usr/bin/env python
+# coding: utf-8
 
-#For example, 6 looks like 9 flipped upside-down. And “69” looks like “96” when held upside down.
+# In[3]:
 
-#Write an algorithm to determine if given a number if it’s possible that the card is displayed upside-down.
 
-#The cards do not have leading 0s for numbers with fewer than 3 digits, so they are not printed “01” for “1” or “071” for “71”  
+# You are managing an auction with 650 bidders, each bidder is given a bidding card with their number printed on it. However, the numbers 0,1,6,8,9 all look the same upside-down or right-side up.
+# For example, “81” looks like “18” when held upside down.
+# Write an algorithm to determine if given a number if it’s 
+# possible that the card is displayed upside-down.
+# The cards do not have leading 0s for numbers with fewer than 3 digits, 
+# so they are not printed “01” for “1” or “071” for “71”  
+
+from IPython.core.debugger import set_trace
+
+flippable_digits = ['0','1','6','8','9']
+
+# recursive function to iterate through digits in num return false if there
+# is a non-flippable digit
+def flip_digit(digit, str_num_arr):
+#     set_trace()
+    if(digit > len(str_num_arr)-1):
+        return True
+    elif(str_num_arr[digit] not in flippable_digits):
+        return False 
+    return flip_digit(digit+1, str_num_arr)
+        
+def can_card_be_flipped(num):
+#     set_trace()
+    string_num = str(num) #convert number to string
+    str_num_arr = list(string_num) # '69' => ['6','9']
+    
+    # base case (reverse the string and see if it starts with 0)
+    if(str_num_arr[::-1][0] == '0'):
+        return False
+    
+    reversed_as_int = int(''.join([str(c) for c in str_num_arr[::-1]]))
+    
+    if(reversed_as_int > 650):
+        return False
+    
+    return flip_digit(0, str_num_arr)
+
+# assume inputs are less than 650
+# test_cases = (161, 365, 72, 68, 1, 11, 69, 96, 100)
+
+# set_trace()
+for num in range(1,651):
+    print("{0}: {1}".format(num, can_card_be_flipped(num)))
+
--- 02.22.19/rock_paper_scissors/rock_paper_scissors.py
+++ 02.22.19/rock_paper_scissors/rock_paper_scissors.py
@@ -1,4 +1,15 @@
-# Let’s play a game. Write out a simple version of “Rock, Paper, Scissors” in the language of your choice. The game should allow for two “players”, it should print out the results of each round, and the game will be completed when a player has won a minimum of four rounds. The rules are as follows: 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Let’s play a game. Write out a simple version of “Rock, Paper, 
+# Scissors” in the language of your choice. 
+# The game should allow for two “players”, it should print 
+# out the results of each round, and the game will be completed 
+# when a player has won a minimum of four rounds. 
+# The rules are as follows: 
 
 #Rock beats Scissors
 #Scissors beats Paper
@@ -6,3 +17,32 @@
 
 #Source: 
 #https://practice.geeksforgeeks.org/problems/rock-paper-scissors/0
+from random import randint
+
+from IPython.core.debugger import set_trace
+
+def play_game():
+#     set_trace()
+    p1Wins = 0
+    p2Wins = 0
+    counter = 0
+    while(p1Wins < 4 and p2Wins < 4):
+#         set_trace()
+        player1Roll = randint(0,2)
+        player2Roll = randint(0,2)
+        if player1Roll == player2Roll:
+            print("Tie...")
+        elif player1Roll is 0 and player2Roll is 2 or player1Roll is 1 and player2Roll is 0 or player1Roll is 2 and player2Roll is 1:
+            print("Player 1 wins...")
+            p1Wins += 1
+            
+        elif player2Roll is 0 and player1Roll is 2 or player2Roll is 1 and player1Roll is 0 or player2Roll is 2 and player1Roll is 1:
+            print("Player 2 wins...")
+            p2Wins += 1
+            
+        counter+=1
+        
+    print("P1: {0}, P2: {1}".format(p1Wins, p2Wins))
+    
+play_game()
+
